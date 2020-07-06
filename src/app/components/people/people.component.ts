c93ad3c321eeab9977ada8c9e9851610a5469397
rahim iqbal <superiqu@gmail.com>
2020-07-06 15:05:50 +0600

    hide loged user from people

--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/services/users.service';
-
+import _ from 'lodash';
+import { TokenService } from 'src/app/services/token.service';
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html',
@@ -8,12 +9,15 @@ import { UsersService } from 'src/app/services/users.service';
 })
 export class PeopleComponent implements OnInit {
   users = [];
+  loggedInUser: any;
 
   constructor(
-    private userService: UsersService
+    private userService: UsersService,
+    private tokenService: TokenService
   ) { }
 
   ngOnInit(): void {
+    this.loggedInUser = this.tokenService.GetPayload();
     this.GetUsers();
   }
 
@@ -21,6 +25,7 @@ export class PeopleComponent implements OnInit {
     this.userService.GetAllUsers()
       .subscribe(data => {
         console.log(data);
+        _.remove(data.result, { username: this.loggedInUser.username })
         this.users = data.result;
         console.log(this.users);
       });
