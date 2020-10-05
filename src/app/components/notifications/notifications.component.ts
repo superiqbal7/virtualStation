d3893f62cfbf2038b9d3a2cd82dc29f7d2afb700
rahim iqbal <superiqu@gmail.com>
2020-10-05 12:19:18 +0600

    Add funtionality to notification view model

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -1,5 +1,7 @@
+import { TokenService } from 'src/app/services/token.service';
 import { Component, OnInit } from '@angular/core';
-
+import io from 'socket.io-client';
+import { UsersService } from 'src/app/services/users.service';
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsComponent implements OnInit {
 
-  constructor() { }
+  socket: any;
+  user: any;
+  notifications = [];
+
+  constructor(private tokenService: TokenService, private userService: UsersService) {
+    this.socket = io('http://localhost:3000');
+   }
 
   ngOnInit() {
+    this.user = this.tokenService.GetPayload();
+    this.GetUser();
+    this.socket.on('refreshPage', ()=>{
+      this.GetUser();
+    })
+  }
+
+  GetUser(){
+    this.userService.GetUserById(this.user._id).subscribe(data => {
+      console.log(data);
+    })
   }
 
 }
