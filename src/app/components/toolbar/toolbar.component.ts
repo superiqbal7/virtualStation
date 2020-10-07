6caba88b5a4e73737bba25087bab456ba2bab3aa
rahim iqbal <superiqu@gmail.com>
2020-10-07 18:14:30 +0600

    update get nofications and implement socket.io

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { TokenService } from 'src/app/services/token.service';
 import { Router } from '@angular/router';
 import * as M from 'materialize-css';
-
+import { UsersService } from 'src/app/services/users.service';
+import * as moment from 'moment';
+import io from 'socket.io-client';
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
@@ -10,15 +12,21 @@ import * as M from 'materialize-css';
 })
 export class ToolbarComponent implements OnInit {
   user: any;
+  notifications = [];
+  socket: any;
 
   constructor(
     private tokenService: TokenService,
-    protected router: Router
-  ) { }
+    protected router: Router,
+    private userService: UsersService
+  ) {
+    this.socket = io('http://localhost:3000');
+   }
 
   ngOnInit() {
     this.user = this.tokenService.GetPayload();
     //console.log(this.user);
+
 
     const dropDownElement = document.querySelector('.dropdown-trigger');
     M.Dropdown.init(dropDownElement, {
@@ -26,7 +34,19 @@ export class ToolbarComponent implements OnInit {
       hover: true,
       coverTrigger: false
     })
+
+    this.GetNotifications();
+    this.socket.on('refreshPage', () => {
+      this.GetNotifications();
+    })
   }
+
+  GetNotifications(){
+    this.userService.GetUserById(this.user._id).subscribe(data => {
+      this.notifications = data.result.notifications.reverse();
+    });
+  }
+
   logout() {
     this.tokenService.DeleteToken()
     this.router.navigate(['']);
@@ -36,4 +56,13 @@ export class ToolbarComponent implements OnInit {
     this.router.navigate(['streams']);
   }
 
+  TimeFromNow(time){
+    return moment(time).fromNow();
+  }
+
+  MarkAll() {
+    this.userService.MarkAllAsRead().subscribe(data => {
+      this.socket.emit('refresh', {});
+    });
+  }
 }
