d8faa05f39ec873b5b01be4d8c913355124141e7
rahim iqbal <superiqu@gmail.com>
2020-10-20 13:02:38 +0600

    update to get user information and implement socket

--- src/app/components/side/side.component.ts
+++ src/app/components/side/side.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/services/token.service';
+import { UsersService } from 'src/app/services/users.service';
+import io from 'socket.io-client';
 
 @Component({
   selector: 'app-side',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideComponent implements OnInit {
 
-  constructor() { }
+  socket: any;
+  user: any;
+  userData: any;
+
+  constructor(
+    private tokenService: TokenService,
+    private userService: UsersService
+  ) {
+    this.socket = io('http://localhost:3000');
+   }
 
   ngOnInit() {
+    this.user = this.tokenService.GetPayload();
+    this.GetUser();
+    this.socket.on('refreshPage', ()=>{
+      this.GetUser();
+    })
+  }
+
+  GetUser(){
+    this.userService.GetUserById(this.user._id).subscribe(data => {
+      this.userData = data.result;
+      console.log(this.userData);
+
+    })
   }
 
 }
