cac010d2cd447ceceb0953932f773c3c9297510f
rahim iqbal <superiqu@gmail.com>
2020-10-24 23:37:35 +0600

    updated comment component data

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { PostService } from 'src/app/services/post.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import io from 'socket.io-client';
 import * as moment from 'moment';
+import _ from 'lodash'
+import { TokenService } from 'src/app/services/token.service';
+
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
@@ -12,21 +15,23 @@ import * as moment from 'moment';
 export class CommentsComponent implements OnInit, AfterViewInit {
   toolbarElement: any;
   socket: any;
-
+  user: any;
   commentForm: FormGroup;
   postId: any;
   commentsArray = [];
-  post: string;
+  post: any;
 
   constructor(
     private formbuilder: FormBuilder,
     private postService: PostService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private tokenService: TokenService
   ) {
     this.socket = io('http://localhost:3000')
    }
 
   ngOnInit() {
+    this.user = this.tokenService.GetPayload();
     this.toolbarElement = document.querySelector('.nav-content');
     this.postId = this.route.snapshot.paramMap.get('id');
     this.init();
@@ -59,7 +64,9 @@ export class CommentsComponent implements OnInit, AfterViewInit {
   GetPost() {
     this.postService.getPost(this.postId).subscribe(data => {
       //console.log(" okay ", data);
-      this.post = data.post.post;
+      this.post = data.post;
+      console.log(data.post);
+
       this.commentsArray = data.post.comments.reverse();
       //console.log(this.commentsArray);
     })
@@ -67,6 +74,18 @@ export class CommentsComponent implements OnInit, AfterViewInit {
 
   TimeFromNow(time) {
     return moment(time).fromNow();
+  }
+
+  LikePost(post) {
+    this.postService.addLike(post).subscribe(data => {
+      console.log(data);
+      this.socket.emit('refresh', {});
+    }, err => console.log(err)
+    );
+  }
+
+  CheckInLikesArray(arr, username) {
+    return _.some(arr, { username: username });
   }
 
 }
