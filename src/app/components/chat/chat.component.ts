4ddf1a1513f0fe7aee1fa7fcc68f9d532e514b07
rahim iqbal <superiqu@gmail.com>
2020-10-30 01:48:54 +0600

    add chat component

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit, AfterViewInit {
+
+  tabElement: any;
+
+  constructor() { }
+
+
+  ngOnInit() {
+    this.tabElement = document.querySelector('.nav-content');
+  }
+
+  ngAfterViewInit(): void {
+    this.tabElement.style.display = 'none';
+  }
+
+}
