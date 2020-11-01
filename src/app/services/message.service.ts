cc2fed988eca3d19836431aaf7b29d655df6b519
rahim iqbal <superiqu@gmail.com>
2020-11-02 00:16:31 +0600

    add mark message and all message feature searvice

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -22,4 +22,12 @@ export class MessageService {
   GetAllMessages(senderId, receiverId): Observable<any> {
     return this.http.get(`${BASEURL}/chat-messages/${senderId}/${receiverId}`);
   }
+
+  MarkMessages(sender, receiver): Observable<any>{
+    return this.http.get(`${BASEURL}/receiver-messages/${sender}/${receiver}`);
+  }
+
+  MarkAllMessages(): Observable<any> {
+    return this.http.get(`${BASEURL}/mark-all-messages`);
+  }
 }
