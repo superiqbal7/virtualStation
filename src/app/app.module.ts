11c7ddfe0c95fefc68380aa6156fdbc2904885c7
rahim iqbal <superiqu@gmail.com>
2020-11-04 02:36:35 +0600

    import chat module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,25 +9,30 @@ import { StreamsModule } from './modules/streams/streams.module';
 import { StreamsRoutingModule } from './modules/streams-routing/streams-routing.module';
 import { CookieService } from 'ngx-cookie-service';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-
+import { EmojiFrequentlyService, PickerModule } from '@ctrl/ngx-emoji-mart';
+import { ChatModule } from './modules/chat/chat.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AuthModule,
     AuthRoutingModule,
     StreamsModule,
-    StreamsRoutingModule
+    StreamsRoutingModule,
+    PickerModule,
+    ChatModule
   ],
   providers: [CookieService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptor,
       multi: true
-    }],
+    },
+    EmojiFrequentlyService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
