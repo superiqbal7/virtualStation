750a454dac0568f965f89aeaff78083156962f6c
rahim iqbal <superiqu@gmail.com>
2020-11-11 10:40:10 +0600

    add userservice

--- src/app/components/images/images.component.ts
+++ src/app/components/images/images.component.ts
@@ -1,6 +1,5 @@
 
 import { Component, OnInit } from '@angular/core';
-import { read } from 'fs';
 import { FileUploader } from 'ng2-file-upload';
 import { UsersService } from 'src/app/services/users.service';
 import { resolve } from 'url';
@@ -22,41 +21,42 @@ export class ImagesComponent implements OnInit {
   selectedFile: any;
 
   constructor(
-    private userService: UsersService
+    private usersService: UsersService
   ) { }
 
   ngOnInit() {
   }
 
-  OnFileSelected(event){
+  OnFileSelected(event) {
     const file: File = event[0];
 
-    this.ReadAsBase64(file).then(result => {
-      this.selectedFile - result;
-    }).catch(err => console.log(err)
-    )
+    this.ReadAsBase64(file)
+      .then(result => {
+        this.selectedFile = result;
+      })
+      .catch(err => console.log(err));
   }
 
-  Upload(){
-    if(this.selectedFile){
-      this.userService.AddImage(this.selectedFile).subscribe(data => {
-        console.log(data);
-        const filePath = <HTMLInputElement>document.getElementById('filePath');
-        filePath.value = '';
-      }, err => {
-        console.log(err);
-      })
+  Upload() {
+    if (this.selectedFile) {
+      this.usersService.AddImage(this.selectedFile).subscribe(
+        data => {
+          const filePath = <HTMLInputElement>document.getElementById('filePath');
+          filePath.value = '';
+        },
+        err => console.log(err)
+      );
     }
   }
 
-  ReadAsBase64(file): Promise<any>{
+  ReadAsBase64(file): Promise<any> {
     const reader = new FileReader();
-    const fileValue = new Promise((resolve, reject)=>{
-      reader.addEventListener('load', ()=>{
+    const fileValue = new Promise((resolve, reject) => {
+      reader.addEventListener('load', () => {
         resolve(reader.result);
       });
 
-      reader.addEventListener('error', (event)=>{
+      reader.addEventListener('error', event => {
         reject(event);
       });
 
