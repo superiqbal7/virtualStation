e6e7e6b3089a6e2a95225db3f5fae72aa61798fc
rahim iqbal <superiqu@gmail.com>
2020-12-06 23:46:23 +0600

    fix tabElement

--- src/app/components/view-user/view-user.component.ts
+++ src/app/components/view-user/view-user.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-view-user',
   templateUrl: './view-user.component.html',
   styleUrls: ['./view-user.component.css']
 })
-export class ViewUserComponent implements OnInit {
+export class ViewUserComponent implements OnInit, AfterViewInit  {
+
+  tabElement: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.tabElement = document.querySelector('.nav-content');
+  }
+
+  ngAfterViewInit(){
+    this.tabElement.style.display = 'none';
   }
 
 }
